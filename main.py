f11c52f925a5c7db754c9a2fd681de202eb4caba
Julieta Brussa <jbrussa01@gmail.com>
2025-06-11 18:57:50 -0300

    schema testeado

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from classes.SQLite_service import SQLite_service
 app = FastAPI()
 client = OpenAI()
 bot = BOT()
+sqlite_service = SQLite_service("database/Chinook_Sqlite.sqlite")
 
 #Endpoints
 @app.get("/ping")
@@ -19,5 +20,5 @@ async def query(query: str = Query(..., description="Pregunta para el bot")):
 
 @app.get("/schema", response_model=dict[str, list[dict[str, str]]])
 async def get_schema():
-    schema = SQLite_service.get_schema()
+    schema = sqlite_service.get_schema()
     return schema
